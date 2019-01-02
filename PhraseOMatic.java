d571067a11f3fac1a371d7472ea257eb35a897aa
Alex Jivov <alexjivov@gwu.edu>
2019-01-02 14:47:30 -0500

    base class and java file created - build out methods and statements

---
+++ PhraseOMatic.java
@@ -0,0 +1,6 @@
+public class PhraseOMatic {
+    public static void main(Stringp[] args) {
+        // three sets of words to choose from
+        String[] wordListOne = {"Big", "Little","Small","Tiny","Gargantuan",};
+    }
+}
